df6a49d97007e5be90338fc5b9908bf8f7736ca8
Simon <sema92m@wp.pl>
2023-05-07 21:23:54 -0700

    feat: change 04-copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,30 +1,20 @@
 const fs = require("fs");
 const path = require("path");
+const directoryPathCopy = path.join(__dirname, "files-copy");
+const folderPath = path.join(__dirname, "files");
 
-function copyDir(source, destination) {
-    if (fs.existsSync(destination)) {
-        deleteFilesInFolder("./04-copy-directory/files-copy");
-    } else if (!fs.existsSync(destination)) {
-        fs.mkdirSync(destination);
-    }
-    const files = fs.readdirSync(source);
-    files.forEach((file) => {
-        const sourcePath = path.join(source, file);
-        const destinationPath = path.join(destination, file);
-        fs.copyFileSync(sourcePath, destinationPath);
+function del() {
+    fs.rm(directoryPathCopy, { recursive: true, force: true }, (err) => {
+        if (err) throw err;
+        console.log("del");
+        create();
     });
 }
-copyDir("./04-copy-directory/files", "./04-copy-directory/files-copy");
-
-function deleteFilesInFolder(folderPath) {
-    const files = fs.readdirSync(folderPath);
-    files.forEach((file) => {
-        const filePath = path.join(folderPath, file);
-        const stats = fs.lstatSync(filePath);
-        if (stats.isDirectory()) {
-            deleteFilesInFolder(filePath);
-        } else {
-            fs.unlinkSync(filePath);
-        }
+function create() {
+    fs.cp(folderPath, directoryPathCopy, { recursive: true }, (err) => {
+        if (err) throw err;
+        console.log("created");
     });
 }
+
+fs.access(directoryPathCopy, (err) => (err ? create() : del()));
