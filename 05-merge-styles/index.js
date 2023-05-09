aff31f13a7c313752602671bd437c5d477561fa2
Simon <sema92m@wp.pl>
2023-05-09 17:01:50 +0200

    feat: change 05-merge-styles solution (no Sync method)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,22 +1,47 @@
 const fs = require("fs");
 const path = require("path");
 const { stdout } = process;
-function mergeCssFiles() {
+
+async function mergeCssFiles() {
     const cssDir = path.join(__dirname, "styles");
     const outputPath = path.join(__dirname, "project-dist/bundle.css");
-    fs.readdir(cssDir, (err, files) => {
-        if (err) throw err;
+    try {
+        const files = await fs.promises.readdir(cssDir);
         const cssFiles = files.filter((file) => path.extname(file) === ".css");
-        const cssContent = cssFiles.map((file) =>
-            fs.readFileSync(path.join(cssDir, file), "utf-8")
+        const cssContent = await Promise.all(
+            cssFiles.map(async (file) => {
+                const cssFileContent = await fs.promises.readFile(
+                    path.join(cssDir, file),
+                    "utf-8"
+                );
+                return cssFileContent;
+            })
         );
-        fs.writeFile(outputPath, cssContent.join("\n"), (err) => {
-            if (err) throw err;
-            stdout.write(
-                "bundle.css created, CSS files from  ./05-merge-styles merged to the ./05-merge-stylesproject-dist/bundle.css"
-            );
-        });
-    });
+        await fs.promises.writeFile(outputPath, cssContent.join("\n"));
+        stdout.write(
+            "bundle.css created, CSS files from  ./05-merge-styles merged to the ./05-merge-stylesproject-dist/bundle.css"
+        );
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 mergeCssFiles();
+
+// function mergeCssFiles() {
+//     const cssDir = path.join(__dirname, "styles");
+//     const outputPath = path.join(__dirname, "project-dist/bundle.css");
+//     fs.readdir(cssDir, (err, files) => {
+//         if (err) throw err;
+//         const cssFiles = files.filter((file) => path.extname(file) === ".css");
+//         const cssContent = cssFiles.map((file) =>
+//             fs.readFileSync(path.join(cssDir, file), "utf-8")
+//         );
+//         fs.writeFile(outputPath, cssContent.join("\n"), (err) => {
+//             if (err) throw err;
+//             stdout.write(
+//                 "bundle.css created, CSS files from  ./05-merge-styles merged to the ./05-merge-stylesproject-dist/bundle.css"
+//             );
+//         });
+//     });
+// }
