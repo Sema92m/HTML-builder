2de6e3738048cd189b2f12d1c91c7fe29254c060
Simon <sema92m@wp.pl>
2023-05-10 20:12:05 +0200

    feat:implement styles merg function

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,43 +1,47 @@
 const fs = require("fs");
 const path = require("path");
 let { COPYFILE_EXCL } = fs.constants;
+
 let header;
 let articles;
 let footer;
 let result;
+
+/*Я не знаю почему, но почти всегда полностью не мержатся стили(например футер и маин мержатся
+а хедер не хочет). Если не соберётся с первого раза попробуйте удалить папу project-dist и попробовать еще раз,
+иногда только после 3го раза создаёт без ошибок. Спасибо!*/
 
 fs.mkdir("06-build-page/project-dist", (err) => {
     if (err) throw err;
     console.log("project-dist created");
 });
 
-function createMainDists() {
-    fs.mkdir("06-build-page/project-dist/assets", (err) => {
-        if (err) throw err;
-        console.log("assets created");
-    });
-}
-createMainDists();
+fs.mkdir("06-build-page/project-dist/assets", (err) => {
+    if (err) throw err;
+    console.log("assets created");
+});
+readHTMLComponents();
+createAssetsDists();
+copyAssetsFiles();
+replaceComponentsToIndexHTML();
+copyStyles();
 
 function createAssetsDists() {
     fs.mkdir("06-build-page/project-dist/assets/fonts", (err) => {
         if (err) throw err;
-        console.log("assets/fonts created");
     });
 
     fs.mkdir("06-build-page/project-dist/assets/img", (err) => {
         if (err) throw err;
-        console.log("assets/img created");
     });
 
     fs.mkdir("06-build-page/project-dist/assets/svg", (err) => {
         if (err) throw err;
-        console.log("assets/svg created");
     });
+    console.log("fonts & img & svg dirs created");
 }
-createAssetsDists();
 
-function readComponents() {
+function readHTMLComponents() {
     fs.readFile(
         "06-build-page/components/header.html",
         "utf-8",
@@ -66,16 +70,13 @@ function readComponents() {
     );
 }
 
-readComponents();
-
-function copyFiles() {
+function copyAssetsFiles() {
     fs.copyFile(
         "06-build-page/assets/fonts/Karolina-Regular.woff2",
         "06-build-page/project-dist/assets/fonts/Karolina-Regular.woff2",
         COPYFILE_EXCL,
         (err) => {
             if (err) throw err;
-            console.log("Fonts kopied");
         }
     );
     fs.readdir("06-build-page/assets/img", (err, files) => {
@@ -86,7 +87,6 @@ function copyFiles() {
                 COPYFILE_EXCL,
                 (err) => {
                     if (err) throw err;
-                    console.log("Img copied");
                 }
             );
         });
@@ -100,45 +100,12 @@ function copyFiles() {
                 COPYFILE_EXCL,
                 (err) => {
                     if (err) throw err;
-                    console.log("svg copied");
                 }
             );
         });
     });
+    console.log("fonts img svg files kopied");
 }
-copyFiles();
-
-function copyStyles() {
-    fs.readdir("06-build-page/styles", (err, files) => {
-        files.forEach((file) => {
-            fs.stat("06-build-page/styles/" + file, (err, stats) => {
-                if (err) {
-                    err;
-                }
-                stats.isFile();
-                path.extname(file);
-                if (stats.isFile() == true && path.extname(file) == ".css") {
-                    fs.readFile(
-                        "06-build-page/styles/" + file,
-                        "utf-8",
-                        (err, data) => {
-                            if (err) throw err;
-                            fs.writeFile(
-                                "06-build-page/project-dist/style.css",
-                                data + "\n",
-                                (err) => {
-                                    if (err) throw err;
-                                    console.log("Styles created");
-                                }
-                            );
-                        }
-                    );
-                }
-            });
-        });
-    });
-}
-copyStyles();
 
 function replaceComponentsToIndexHTML() {
     fs.readFile("06-build-page/template.html", "utf-8", (err, data) => {
@@ -153,9 +120,40 @@ function replaceComponentsToIndexHTML() {
             "utf-8",
             function (err) {
                 if (err) throw err;
-                console.log("project-dist/index.html created");
+                console.log("index.html created & apply components");
             }
         );
     });
 }
-replaceComponentsToIndexHTML();
+
+function copyStyles() {
+    fs.readdir("06-build-page/styles", (err, files) => {
+        if (err) throw err;
+        let cssContents = "";
+        files.forEach((file) => {
+            fs.stat("06-build-page/styles/" + file, (err, stats) => {
+                if (err) throw err;
+                if (stats.isFile() && path.extname(file) == ".css") {
+                    fs.readFile(
+                        "06-build-page/styles/" + file,
+                        "utf-8",
+                        (err, data) => {
+                            if (err) throw err;
+                            cssContents += data + "\n";
+                            if (files.indexOf(file) === files.length - 1) {
+                                fs.writeFile(
+                                    "06-build-page/project-dist/style.css",
+                                    cssContents,
+                                    (err) => {
+                                        if (err) throw err;
+                                        console.log("Styles file  created");
+                                    }
+                                );
+                            }
+                        }
+                    );
+                }
+            });
+        });
+    });
+}
