0c369421b60e8e8b59cb9c0deb9b54432aa5cf7c
Simon <sema92m@wp.pl>
2023-05-10 17:56:40 +0200

    feat: change  first task solution

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,4 @@
 const fs = require("fs");
-const path = require("path");
-fs.readFile(path.join("01-read-file", "text.txt"), "utf-8", (err, data) => {
-    if (err) throw err;
-    console.log(data);
-});
+
+const readStream = fs.createReadStream("./01-read-file/text.txt", "utf-8");
+readStream.pipe(process.stdout);
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -27,21 +27,3 @@ async function mergeCssFiles() {
 }
 
 mergeCssFiles();
-
-// function mergeCssFiles() {
-//     const cssDir = path.join(__dirname, "styles");
-//     const outputPath = path.join(__dirname, "project-dist/bundle.css");
-//     fs.readdir(cssDir, (err, files) => {
-//         if (err) throw err;
-//         const cssFiles = files.filter((file) => path.extname(file) === ".css");
-//         const cssContent = cssFiles.map((file) =>
-//             fs.readFileSync(path.join(cssDir, file), "utf-8")
-//         );
-//         fs.writeFile(outputPath, cssContent.join("\n"), (err) => {
-//             if (err) throw err;
-//             stdout.write(
-//                 "bundle.css created, CSS files from  ./05-merge-styles merged to the ./05-merge-stylesproject-dist/bundle.css"
-//             );
-//         });
-//     });
-// }
